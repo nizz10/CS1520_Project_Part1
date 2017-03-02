aa883f6289422d6714d7a40fee0e898085b63077
nizz10 <Ningqian@Ningqians-MacBook-Pro.local>
2017-03-01 19:59:14 -0500

    Added new pages and images, modified css and js

---
+++ js/script.js
@@ -0,0 +1,44 @@
+
+function displayMessage(){
+  var inputs = document.getElementsByTagName("input");
+
+
+  for(var i = 0; i < inputs.length-1; i++)
+  {
+    if(inputs[i].value == "Required" || inputs[i].value == "")
+    {
+      alert("Please fill the blank " + inputs[i].name);
+      inputs[i].value = "";
+      inputs[i].focus();
+      break;
+    }
+    if(inputs[i].name == "email" || inputs[i].name == "confirmemail")
+    {
+      if(!inputs[i].value.includes("@"))
+      {
+        alert("Please enter a valid email address with \"@\" in " + inputs[i].name);
+        inputs[i].focus();
+        break;
+      }
+      else if(inputs[i].value.charAt(0) == "@")
+      {
+        alert("Please enter a part followed by \"@\" in " + inputs[i].name);
+        inputs[i].focus();
+        break;
+      }
+      else if(inputs[i].value.charAt(inputs[i].value.length-1) == "@")
+      {
+        alert("Please enter a part following \"@\" in " + inputs[i].name);
+        inputs[i].focus();
+        break;
+      }
+
+    }
+
+
+    if(i == inputs.length-2)
+    {
+      document.body.innerHTML = 'Your message is successfully stored!';
+    }
+  }
+}
